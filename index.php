36ef004fc9f6a05a8c3bfdfc86377512d1bff2a7
ivan maulana <ivanmaulana@apps.ipb.ac.id>
2016-08-08 06:47:27 +0700

    initial commit

---
+++ index.php
@@ -0,0 +1,392 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="description" content="">
+<meta name="author" content="">
+<link rel="shortcut icon" href="assets/img/nusantara.png">
+<title>NU-santara KMNU</title>
+
+
+<!-- Bootstrap core CSS -->
+<link href="assets/css/bootstrap.css" rel="stylesheet">
+
+<!-- Custom CSS -->
+<link href="assets/css/main.css" rel="stylesheet">
+<link href="assets/css/font-awesome.min.css" rel="stylesheet">
+<link href="assets/css/animate-custom.css" rel="stylesheet">
+<link href='http://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Raleway:400,300,700' rel='stylesheet' type='text/css'>
+<script src="assets/js/jquery.min.js"></script>
+<script type="text/javascript" src="assets/js/modernizr.custom.js"></script>
+
+<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body data-spy="scroll" data-offset="0" data-target="#navbar-main">
+<div id="navbar-main"> 
+  <!-- Fixed navbar -->
+  <div class="navbar navbar-default navbar-fixed-top">
+    <div class="container">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse"> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
+        <a style="color:#fff" class="navbar-brand" href="#home"><img height="35px" src="assets/img/nusantara.png"> &nbsp; NU-santara KMNU</a> </div>
+      <div class="navbar-collapse collapse">
+        <ul class="nav navbar-nav navbar-right">
+          <li><a href="#home" class="smoothScroll">Home</a></li>
+          <li> <a href="#tentang" class="smoothScroll"> Tentang</a></li>
+          <li> <a href="#lomba" class="smoothScroll"> Lomba</a></li>
+          <li> <a href="#pelatihan" class="smoothScroll"> Pelatihan</a></li>
+          <li> <a href="#forum" class="smoothScroll"> Forum</a></li>
+          <li> <a href="#seminar" class="smoothScroll"> Seminar</a></li>
+          <li> <a href="#panduan" class="smoothScroll"> Unduh Panduan</a></li>
+          <li> <a href="#kontak" class="smoothScroll"> Kontak</a></li>
+        </ul>
+      </div>
+      <!--/.nav-collapse --> 
+    </div>
+  </div>
+</div>
+
+<!-- ==== HEADERWRAP ==== -->
+<div id="headerwrap" name="home">
+  <header class="clearfix">
+    <img height="200px" src="assets/img/nusantara.png">
+    <h1>NU-santara KMNU</h1><hr>
+    <p><b>"Nahdlatul Ulama - Science and Cultural Art Olympiad"</b></p>
+    <a href="#tentang"class="smoothScroll btn btn-lg"><i>Institut Pertanian Bogor, 16-18 Oktober 2015</i></a>   </header>
+</div>
+<!-- /headerwrap --> 
+
+<!-- ==== ABOUT ==== -->
+<div id="about" name="tentang">
+  <div class="container">
+  <div class="row">
+      <h2 class="centered"><b>Tentang NU-santara KMNU 2015</b></h2>
+      <hr>
+  </div>
+    <div class="row white">
+      <div class="col-md-6 feature-2 wp2 delay-05s"> <img class="img-responsive" src="assets/img/nusantara.png" align=""> </div>
+      <div class="col-md-6 feature-2 wp2 delay-1s">
+        <h3><a><i>Tentang NU-santara 2015</i></a></h3>
+        <p>Suatu rangkaian kegiatan yang terdiri dari beberapa Perlombaan Tingkat Nasional, Pelatihan Nasional, Forum Pembina KMNU Nasional serta Seminar Kebangsaan dan Kontemplasi Budaya.<br><br>
+        Kegiatan ini merupakan salah satu bentuk kegiatan yang mewadahi dan memfasilitasi pemikiran-pemikiran ilmiah para mahasiswa Nahdlatul Ulama, potensi non akademik bidang keagamaan dan pelestarian tradisi serta sebagai bentuk apresiasi terhadap prestasi para mahasiswa Nahdlatul 'Ulama.</p><br>
+        <h3><a><i>Apa Tujuan NU-santara 2015 ?</i></a></h3>
+        <p>NU-santara 2015 bertujuan untuk menggali, memfasilitasi potensi, serta pengapresiasi prestasi para mahasiswa Nahdlatul Ulama.</p>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-lg-6 col-lg-offset-3 centered">
+      <p><h3><b><i><a>Tempat :</a> Institut Pertanian Bogor</i></b></h3></p>
+      <p><h3><b><i><a>Waktu :</a> 16 - 18 Oktober 2015</i></b></h3></p>
+      </div>
+      </div>
+    <!-- row --> 
+  </div>
+</div>
+<!-- container --> 
+
+<!-- ==== SERVICES ==== -->
+<div id="services" name="lomba">
+  <div class="container">
+    <div class="row">
+      <h2 class="centered"><b>Lomba Tingkat Nasional</b></h2>
+      <hr>
+      <div class="col-lg-8 col-lg-offset-2">
+        <p class="large"><b>Lomba Tingkat Nasional merupakan rangkaian kegiatan NU-santara 2015 yang terdiri atas :</b></p>
+      </div>
+      <div class="col-lg-3 callout"> <i class="fa fa-music fa-3x"></i>
+        <h3><i>Cipta Mars KMNU</i></h3>
+        <p>Perlombaan yang bertujuan meningkatkan rasa memiliki KMNU. Lomba ini juga bertujuan untuk melengkapi atribut KMNU Nasional yaitu berupa Mars KMNU.</p>
+      </div>
+      <div class="col-lg-3 callout"> <i class="fa fa-file-text-o fa-3x"></i>
+        <h3><i>Essay</i></h3>
+        <p>Ide dan gagasan dalam pembuatan Karya Tulis ini haruslah orisinil dan berkaitan dengan kebermanfatan IPTEK dalam kehidupan nyata di masyarakat.</p>
+      </div>
+      <div class="col-lg-3 callout"> <i class="fa fa-book fa-3x"></i>
+        <h3><i>Qiroatul Kutub</i></h3>
+        <p>Salah satu perlombaan yang bertujuan melestarikan tradisi membaca kitab kuning bagi kalangan mahasiswa yang dulunya pernah belajar di pesantren.</p>
+      </div>
+      <div class="col-lg-3 callout"> <i class="fa fa-circle-o-notch  fa-3x"></i>
+        <h3><i>Hadroh</i></h3>
+        <p>Melestarikan kesenian hadroh yang bernafaskan sholawat kepada Nabi Muhammmad SAW. Salah satu bentuk akulturasi budaya yang digunakan para ulama pendahulu sebagai media dakwah Islam.</p>
+      </div>
+                
+    </div>
+    <!-- row --> 
+    <div class="row">
+      <div class="col-md-2"></div>
+      <div class="col-md-8">
+          <h3>Panduan &amp; Ketentuan Lomba NU-santara KMNU 2015 <i><a style="color:#4abcc5" href="assets/file/Panduan NU-santara.rar">Unduh Disini</a></i></h3>
+      </div>
+      <div class="col-md-2"></div>
+    </div>
+  </div>
+</div>
+<!-- container --> 
+
+<!-- ==== PORTFOLIO ==== -->
+<div id="portfolio">
+  <div class="container">
+    <div class="row">
+      <h2 class="centered"><b>Pembukaan NU-santara KMNU</b></h2>
+      <hr>
+      <div class="col-lg-8 col-lg-offset-2 centered">
+        <p class="large">Pembukaan NU-santara 2015 merupakan rangkaian kegiatan <b><a>NU-santara 2015</a></b> yang akan dihadiri oleh 2 tokoh yaitu: </p>
+        <p><b><a><i>Pertama, Prof. Dr Herry Suhardiyanto (Rektor IPB)</i></a></b></p>
+        <p><b><a><i>Kedua, Dr. KH Said Aqil Sirodj (Ketua Umum PBNU)</i></a></b></p><br>
+      </div>
+    </div>
+    <!-- /row -->
+    <div class="container">
+      <div class="row"> 
+        <!-- PORTFOLIO IMAGE 1 -->
+        <div class="col-md-5 ">
+          <div class="grid mask">
+            <figure> <img class="img-responsive" src="assets/img/portfolio/folio01.jpg" alt="">
+              <figcaption>
+                <h5>Prof. Dr. Herry Suhardiyanto</h5>
+                <a data-toggle="modal" class="btn btn-default">Rektor IPB</a> </figcaption>
+              <!-- /figcaption --> 
+            </figure>
+            <!-- /figure --> 
+          </div>
+          <!-- /grid-mask --> 
+        </div>
+        
+        <div class="col-md-2"> </div>
+
+        <!-- PORTFOLIO IMAGE 2 -->
+        <div class="col-md-5">
+          <div class="grid mask">
+            <figure> <img class="img-responsive" src="assets/img/portfolio/folio02.jpg" alt="">
+              <figcaption>
+                <h5>Dr. KH Said Aqil Siradj</h5>
+                <a data-toggle="modal" class="btn btn-default">Ketua Umum PBNU</a> </figcaption>
+              <!-- /figcaption --> 
+            </figure>
+            <!-- /figure --> 
+          </div>
+          <!-- /grid-mask --> 
+        </div>
+      </div>
+      <!-- /row --> 
+        
+         
+      </div>
+      <!-- /row --> 
+    </div>
+    <!-- /row --> 
+  </div>
+</div>
+<!-- /container --> 
+
+<!-- ==== SERVICES ==== -->
+<div id="services" name="pelatihan">
+  <div class="container">
+    <div class="row">
+      <h2 class="centered"><b>Pelatihan Nasional</b></h2>
+      <hr>
+      <div class="col-lg-8 col-lg-offset-2">
+        <p class="large"><b>Pelatihan Nasional hanya dapat diikuti oleh delegasi Keluarga Mahasiswa Nahdlatul Ulama Perguruan Tinggi :</b></p>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-1"></div>
+      <div class="col-lg-2 callout"> <i class="fa fa-check-square-o fa-3x"></i>
+        <h3>Kajian Dakwah Ke-Aswaja-an</h3>
+        <p><i><b>Narasumber :</b><br>- KH. Marzuki Mustamar<br>- Pengurus Pusat Lembaga Dakwah NU</i></p>
+      </div>
+      <div class="col-lg-2 callout"> <i class="fa fa-check-square-o fa-3x"></i>
+        <h3>Jurnalistik dan IT</h3>
+        <p><i><b>Narasumber :</b><br>- Pemimpin Redaksi NU Online<br>- Shavic Pontoh</i></p>
+      </div>
+      <div class="col-lg-2 callout"> <i class="fa fa-check-square-o fa-3x"></i>
+        <h3>Administrasi dan Keuangan</h3>
+        <p><i><b>Narasumber :</b><br>- Presidium Nasional 2<br>- Presidium Nasional 3</i></p>
+      </div>
+      <div class="col-lg-2 callout"> <i class="fa fa-check-square-o  fa-3x"></i>
+        <h3>NuTraining</h3>
+        <p><i><b>Narasumber :</b><br>- Ahmad Subagio (Trainer Internasional )<br>- M. Arifin (Trainer Muda NU)</i></p>
+      </div>
+      <div class="col-lg-2 callout"> <i class="fa fa-check-square-o  fa-3x"></i>
+        <h3>Ke-NU-an dan Ke-KMNU-an</h3>
+        <p><i><b>Narasumber :</b><br>- Presidium Nasional 1<br>- Presidium Nasional 4</i></p>
+      </div>
+      <div class="col-md-1"></div>
+                
+    </div>
+    <!-- row -->
+  </div>
+</div>
+<!-- container --> 
+
+
+<!-- ==== PORTFOLIO ==== -->
+<div id="portfolio" name="forum">
+  <div class="container">
+    <div class="row">
+      <h2 class="centered"><b>Forum Nasional Pembina KMNU</b></h2>
+      <hr>
+      <div class="col-lg-8 col-lg-offset-2 centered">
+        <p class="large">Salah satu forum yang mempertemukan para pembina KMNU dari masing-masing perguruan tinggi agar terbentuk sinergi antar pembina KMNU sehingga dapat memberikan kritik dan saran yang membangun bagi KMNU Nasional dan KMNU Perguruan Tinggi.</p>
+      </div>
+      <div class="col-lg-6 col-lg-offset-3 centered">
+          <div class="grid mask">
+            <figure> <img class="img-responsive" src="assets/img/portfolio/adnan-anwar.jpg" alt="">
+              <figcaption>
+                <h5>Adnan Anwar M.A</h5>
+                <a data-toggle="modal" class="btn btn-default">Wasekjen PBNU 2010-2015</a> </figcaption>
+              <!-- /figcaption --> 
+            </figure>
+            <!-- /figure --> 
+          </div>
+        </div>
+          <!-- /grid-mask --> 
+        <div class="col-lg-8 col-lg-offset-2 centered">
+        <br><p><b><i>Narasumber : <a> Adnan Anwar M.A (Wasekjen PBNU 2010-2015)</i></a></b></p>
+      	</div>
+      </div>
+    </div>
+
+        
+         
+      </div>
+      <!-- /row --> 
+    </div>
+    <!-- /row --> 
+  </div>
+</div>
+<!-- /container --> 
+
+<!-- ==== TEAM MEMBERS ==== -->
+<div id="team" name="seminar">
+  <div class="container">
+    <div class="row centered">
+      <h2 class="centered"><b>Seminar Kebangsaan &amp; Kontemplasi Budaya</b></h2>
+      <hr>
+      <div class="col-lg-8 col-lg-offset-2 centered">
+      <h4><b><i>Tema: “Menyongsong Indonesia yang Prestatif dengan Integrasi IPTEK, IMTAQ, dan Kearifan Budaya Sesuai Khazanah Aswaja”</i></b></h4><br>
+        <p>Kegiatan ini harapan nya dapat memotivasi peserta untuk terus berprestasi dan berkarya sesuai dengan bidang keilmuan masing-masing tanpa melupakan khasanah kebudayaan dan spiritualitas yang telah diajarkan oleh para ulama pendiri bangsa Indonesia.</p>
+      </div>
+    </div>
+    <div class="row centerd">
+    <h4><b><i>Narasumber :</i></b></h4><br>
+    </div>
+    <div class="row centered">
+      <div class="col-lg-2">
+      </div>
+      <div class="col-lg-4 centered">
+          <div class="grid mask">
+            <figure> <img class="img-responsive" src="assets/img/portfolio/kh-zawawi-imron.jpg" alt="">
+              <figcaption>
+                <h5>K.H. Zawawi Imron</h5>
+                <a data-toggle="modal" class="btn btn-default">Penyair</a> </figcaption>
+              <!-- /figcaption --> 
+            </figure>
+            <!-- /figure --> 
+          </div><br>
+          <p><b><i><a> K.H. Zawawi Imron (Penyair)</i></a></b></p>
+        </div>
+      <div class="col-lg-4 centered">
+          <div class="grid mask">
+            <figure> <img class="img-responsive" src="assets/img/portfolio/ki_enthus.jpg" alt="">
+              <figcaption>
+                <h5>Ki Enthus</h5>
+                <a data-toggle="modal" class="btn btn-default">Bupati Tegal</a> </figcaption>
+              <!-- /figcaption --> 
+            </figure>
+            <!-- /figure --> 
+          </div><br>
+          <p><b><i><a>Ki Enthus (Bupati Tegal)</i></a></b></p>
+        </div>
+      <div class="col-lg-2">
+      </div>
+      <div class="row">
+      <div class="col-lg-6 col-lg-offset-3">
+      <p><h3 style="color:white"><b><i>Dan Tokoh Lainnya</i></b></h3></p>
+      </div>
+      </div>
+    </div>
+  </div>
+  <!-- row --> 
+</div>
+<!-- container --> 
+
+
+<div id="portfolio" name="panduan">
+  <div class="container">
+    <div class="row centered">
+      <h2 class="centered"><b>Panduan Lomba</b></h2>
+      <hr>
+      <a href="assets/file/Panduan NU-santara.rar" class="btn btn-default" style="color:white;background-color:teal;font-size:20px"><i> Unduh Panduan &amp; Ketentuan Lomba Disini</i> </a>
+    </div>
+  </div>
+  <!-- row --> 
+</div>
+
+<!-- ==== CONTACT ==== -->
+<div id="team" name="kontak">
+  <div class="container">
+    <div class="row">
+      <h2 class="centered"><b>Organized By :</b></h2>
+      <hr>
+    <div class="col-lg-12">
+        <p style="font-size:20px"><b>Keluarga Mahasiswa Nahdlatul Ulama (KMNU)</b> adalah organisasi kekeluargaan (Forum Silaturrahmi) bagi mahasiswa NU yang berdiri di beberapa Perguruan Tinggi dengan sejarah dan latar belakang yang berbeda.
+        <br><br>Namun, KMNU memiliki tujuan yang sama, yakni untuk menghimpun mahasiswa NU di Perguruan Tinggi di Indonesia maupun Luar Negeri untuk bersama-sama menguatkan Islam Ahlussunnah Wal Jama'ah. Arah perjuangan KMNU sejalan dalam arah perjuangan Nahdlatul Ulama, yaitu sosial keagamaan dengan menyinergikan kemampuan IMTAQ dan IPTEK.
+        <br><br>Profil Lengkap KMNU <a style="color:white" href="http://kmnu.or.id/statis-1-profil.html"><b><i><u>Klik Disini</u></i></b></a></p>
+    </div>
+    </div>
+    <div class="row">
+      <h2 class="centered"><b>Kontak</b></h2>
+      <hr>
+    </div>
+    <div class="row">
+      <div class="col-md-4 centered"> <i class="fa fa-map-marker fa-2x"></i>
+        <p>Jl. Kampus Dalam, Cangkurawok<br>
+        Dramaga, Bogor</p>
+      </div>
+      <div class="col-md-4"> <i class="fa fa-envelope-o fa-2x"></i>
+        <p>nu-santara@kmnu.or.id</p>
+      </div>
+      <div class="col-md-4"> <i class="fa fa-phone fa-2x"></i>
+        <p>085786476492 (Puguh Imam)</p>
+      </div>
+    </div>
+
+  </div>
+</div>
+
+
+<div id="footerwrap">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-8"> <span class="copyright">&copy; 2015 oleh <a href="http://www.kmnu.or.id" rel="nofollow">Tim IT KMNU Nasional</a></span> </div>
+      <div class="col-md-4">
+        <ul class="list-inline social-buttons">
+          <li><a href="http://twitter.com/kmnupusat"><i class="fa fa-twitter"></i></a> </li>
+          <li><a href="https://web.facebook.com/pages/Keluarga-Mahasiswa-Nahdlatul-Ulama/150949951635574"><i class="fa fa-facebook"></i></a> </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- Bootstrap core JavaScript
+    ================================================== --> 
+<!-- Placed at the end of the document so the pages load faster --> 
+
+<script type="text/javascript" src="assets/js/bootstrap.min.js"></script> 
+<script type="text/javascript" src="assets/js/retina.js"></script> 
+<script type="text/javascript" src="assets/js/jquery.easing.1.3.js"></script> 
+<script type="text/javascript" src="assets/js/smoothscroll.js"></script> 
+<script type="text/javascript" src="assets/js/jquery-func.js"></script>
+</body>
+</html>
